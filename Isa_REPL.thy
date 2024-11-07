340274022780c139a8798256a2e36584f5f80cce
Qiyuan Xu <xqyww123@gmail.com>
2024-11-08 00:27:00 +0800

    DONE BUT NOT TESTED

---
+++ Isa_REPL.thy
@@ -0,0 +1,13 @@
+theory "Isa_REPL"
+  imports Main
+begin
+
+ML_file \<open>contrib/mlmsgpack/mlmsgpack-aux.sml\<close>
+ML_file \<open>contrib/mlmsgpack/realprinter-packreal.sml\<close>
+ML_file \<open>contrib/mlmsgpack/mlmsgpack.sml\<close>
+
+ML_file \<open>library/REPL.ML\<close>
+ML_file \<open>library/REPL_serializer.ML\<close>
+ML_file \<open>library/Server.ML\<close>
+
+end
