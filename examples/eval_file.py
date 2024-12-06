e2f68bcbdd505e8efa280b4b0d9a3ae510fff95a
Qiyuan Xu <xqyww123@gmail.com>
2024-12-06 11:26:51 +0800

    Hotfix

--- examples/eval_file.py
+++ examples/eval_file.py
@@ -21,7 +21,7 @@ if len(sys.argv) != 3:
 addr   = sys.argv[1]
 target = sys.argv[2]
 
-c = Client(addr, thy_qualifier='HOL')
+c = Client(addr, 'WebAssembly')
 c.set_trace (False) # I disable the tracing for speeding up the evaluation
                     # Consequently, all c.eval(..)[0] will be None
 
