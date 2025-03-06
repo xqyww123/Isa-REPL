8ad7df3b48fb3218ba56c080bafd10ecab7e9b17
Qiyuan Xu <xqyww123@gmail.com>
2025-03-06 13:46:32 +0800

    WATCH DOG

---
+++ repl_server_watch_dog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This is a simple watch dog that ensures the server is keeping online
+# It has the same parameters as the server.
+
+echo -1000 > /proc/$$/oom_score_adj
+
+while true; do
+    ./repl_server.sh "$@"
+    exit_status=$?
+
+    if [ $exit_status -eq 0 ]; then
+        break
+    fi
+
+    echo "The server terminates with code $exit_status. Now rebooting the server..."
+    sleep 1
+done
