0c866a1b75c6a42ad98e045155f4767fd941c427
Qiyuan Xu <xqyww123@gmail.com>
2025-11-14 19:38:37 +0400

    NO DEBUG

--- Isa_REPL.thy
+++ Isa_REPL.thy
@@ -6,8 +6,6 @@ ML_file \<open>contrib/mlmsgpack/mlmsgpack-aux.sml\<close>
 ML_file \<open>contrib/mlmsgpack/realprinter-packreal.sml\<close>
 ML_file \<open>contrib/mlmsgpack/mlmsgpack.sml\<close>
 
-declare [[ML_debugger]]
-
 ML_file \<open>library/premise_selection.ML\<close>
 ML_file \<open>library/REPL.ML\<close>
 ML_file \<open>library/REPL_serializer.ML\<close>
